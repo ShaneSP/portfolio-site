2182e1bfaacbae8f5341598f4f5cf48bcfe60bd6
unknown <shanesp@live.unc.edu>
2021-02-21 13:26:57 -0500

    Adds some styling to navigation bar and first iteration of the page's css grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
+import "./app.scss";
 
 interface ComponentProps {}
 interface ComponentState {}
@@ -8,9 +9,12 @@ interface ComponentState {}
 class App extends Component<ComponentProps, ComponentState> {
   render() {
     return (
-      <div>
+      <div className="app">
         <Header />
         <Navigation />
+        <div
+          className="content"
+        />
         {/* content */}
       </div>
     );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,39 @@
 import React from "react";
-import "navigation.scss";
+import "./navigation.scss";
 
 export default function Navigation() {
-  return <nav></nav>;
+  return (<nav>
+    <div className="nav-items">
+      <a
+        draggable={false}
+        className="nav-item-wrapper"
+        href="#board"
+      >
+        <div className="nav-item">
+          <span className="nav-item-icon">😋</span>
+          <span className="nav-item-title">Board</span>
+        </div>
+      </a>
+      <a
+        draggable={false}
+        className="nav-item-wrapper"
+        href="#about"
+      >
+        <div className="nav-item">
+          <span className="nav-item-icon">🤣</span>
+          <span className="nav-item-title">About</span>
+        </div>
+      </a>
+      <a
+        draggable={false}
+        className="nav-item-wrapper"
+        href="#contact"
+      >
+        <div className="nav-item">
+          <span className="nav-item-icon">🤠</span>
+          <span className="nav-item-title">Contact</span>
+        </div>
+      </a>
+    </div>
+  </nav>);
 }
